e62f644575eb18d870c07d920ed2f07540b38ca7
Adriano Martins de Oliveira Sousa <adrianomartins002@gmail.com>
2023-10-19 20:05:48 +0200

    start

--- src/services/Users.js
+++ src/services/Users.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://60c4063c2df2cb00178ac12b.mockapi.io';
 export const UsersService = {
     getUsers: async ()=>{
-        return (await axios.get(BASE_URL+'/users')).data;
+        return (await axios.get(process.env.REACT_APP_BASE_URL_USER_API+'/users')).data;
     }
 }
--- src/services/Weather.js
+++ src/services/Weather.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export const WeatherService  = {
     getWeatherByLocation: async (latitude, longitude)=>{
         try{
-            const response = await axios.get(BASE_URL+'/weather',{
+            const response = await axios.get(process.env.REACT_APP_BASE_URL_WEATHER_API+'/weather',{
                 params:{
                  lat: 50,
                  lon: 40,
-                 appid: "47ec3cd1bacd2d461e09c88a375e1f9d"   
+                 appid: process.env.REACT_APP_APPID   
                 }
             })
             return response.data
